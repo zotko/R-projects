eac8dae8cd8458ba48db8a4d1880d878e9a2a7b1
Mykola Zotko <mzotko@gmail.com>
2020-01-07 21:42:24 +0100

    Second commit

--- forests/main.R
+++ forests/main.R
@@ -2,20 +2,25 @@ library(readr)
 library(dplyr)
 library(ggplot2)
 
-install.packages('ggplot2')
+setwd("~/Code/r_projects/forests")
+forest_fires = read_csv('forestfires.csv')
 
-# read file
-forestfires = read_csv('forestfires.csv')
+forest_fires <- forest_fires %>% 
+  mutate(month = factor(month, levels = c("jan", "feb", "mar", "apr", "may", "jun",
+                                          "jul", "aug", "sep", "oct", "nov", "dec")))
+forest_fires <- forest_fires %>% 
+  mutate(day = factor(day, levels = c("mon", "tue", "wed", "thu", "fri", "sat", "sun")))
 
-fires_perday <- forestfires %>%
+fires_perday <- forest_fires %>%
   group_by(day) %>%
   summarise(total_fires = n())
+
 
 ggplot(data = fires_perday) +
   aes(x = day, y = total_fires) +
   geom_bar(stat = 'identity')
 
-fires_permonth <- forestfires %>%
+fires_permonth <- forest_fires %>%
   group_by(month) %>%
   summarise(total_fires = n())
 
